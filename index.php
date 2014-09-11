c75042ac9e4c9cb15591546d6bda4a450ee65879
TommusRhodus <hello@madeinebor.com>
2014-09-11 21:23:40 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Ebor Framework
+Plugin URI: http://www.madeinebor.com/ebor-framework
+Description: Ebor Framework - The Driving Force Behind TommusRhodus Themes
+Version: 1.0.0
+Author: Tom Rhodes
+Author URI: http://www.madeinebor.com
+*/	
