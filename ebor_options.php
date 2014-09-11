896faea251ac2147ca18380c74351392ced2c791
TommusRhodus <hello@madeinebor.com>
2014-09-12 00:26:58 +0100

    More Changes than you can shake a stick at

---
+++ ebor_options.php
@@ -0,0 +1,226 @@
+<?php 
+
+/**
+ * Ebor Options Class
+ * 
+ * A quick and easy way of building theme options.
+ * The WordPress Customization API is really simple to use, but if you have a lot of options, it can create a bit of a mess.
+ * This class aims to fix that by allowing you to add sections and settings with just 1 function call from your themes functions.php file.
+ * 
+ * @author TommusRhodus
+ * @since 1.0.0
+ */
+class Ebor_Options {
+	
+	/**
+	 * Define the arrays we'll use for building our options
+	 */
+	public $panels = array();
+	public $sections = array();
+	public $settings = array();
+	
+	/**
+	 * Define functions called as this class is initialised.
+	 * additional_classes are the custom controls we use in the customiser
+	 * build_options runs through all of our added options and turns them into proper customization API options
+	 */
+	public function __construct(){
+		add_action('customize_register', 'ebor_additional_classes' );
+		add_action('customize_register', array( $this, 'build_options' ) );
+	}
+	
+	public function add_panel($title, $priority, $description = ''){
+		$this->panels[] = array(
+			'title' => $title,
+			'priority' => $priority,
+			'description' => $description
+		);	
+	}
+	
+	/**
+	 * This function adds customizations sections
+	 */
+	public function add_section($name, $title, $priority, $panel = '', $description = ''){
+		$this->sections[] = array(
+			'name' => $name,
+			'title' => $title,
+			'priority' => $priority,
+			'description'=> $description,
+			'panel' => $panel
+		);
+	}
+	
+	/**
+	 * This function adds customization settings
+	 */
+	public function add_setting($type, $name, $title, $section, $default = '', $priority = ''){
+		$this->settings[] = array(
+			'type' => $type,
+			'name' => $name,
+			'title' => $title,
+			'section' => $section,
+			'default' => $default,
+			'priority' => $priority,
+		);
+	}
+	
+	/**
+	 * Finally, run through our array and build them options!
+	 */
+	public function build_options($wp_customize){
+		
+		global $wp_version;
+		
+		/**
+		 * Build Panels
+		 */
+		if( is_array($this->panels) && $wp_version >= 4.0 ){
+			foreach( $this->panels as $panel ){
+				
+				extract($panel);
+				
+				$wp_customize->add_panel( 
+					$title, array(
+						'title'          => $title,
+						'description'    => $description,
+						'priority'       => $priority
+					) 
+				);
+				
+			}
+		}
+		
+		/**
+		 * Build Sections
+		 */
+		if( is_array($this->sections) ){
+			foreach( $this->sections as $section ){
+				
+				extract( $section );
+				
+				$wp_customize->add_section( 
+					$name, array(
+						'title'          => $title,
+						'description'    => $description,
+						'priority'       => $priority,
+						'panel'          => $panel
+					) 
+				);
+				
+			}
+		}
+		
+		/**
+		 * Build Settings
+		 */
+		if( is_array($this->settings) ){
+			foreach( $this->settings as $setting ){
+				
+				extract( $setting );
+				
+				$wp_customize->add_setting(
+					$name, array(
+						'default'  => $default,
+						'type' => 'option'
+					)
+				);
+				
+				if( 'image' == $type ){
+					
+					$wp_customize->add_control( 
+						new WP_Customize_Image_Control(
+							$wp_customize, $name, array(
+					    		'label'    => $title,
+					    		'section'  => $section,
+					    		'priority' => $priority
+							)
+						)
+					);
+					
+				} elseif( 'number' == $type ){
+					
+					$wp_customize->add_control( 
+						new Ebor_Customizer_Number_Control(
+							$wp_customize, $name, array(
+					    		'label'    => $title,
+					    		'section'  => $section,
+					    		'priority' => $priority
+							)
+						)
+					);
+					
+				} elseif( 'textarea' == $type ){
+					
+					$wp_customize->add_control( 
+						new Ebor_Customizer_Textarea_Control(
+							$wp_customize, $name, array(
+					    		'label'    => $title,
+					    		'section'  => $section,
+					    		'priority' => $priority
+							)
+						)
+					);
+					
+				} else {
+					
+					$wp_customize->add_control( 
+						$name, array(
+						    'type' => 'text',
+						    'label'    => $title,
+						    'section'  => $section,
+						    'priority' => $priority
+						) 
+					);
+					
+				}
+				
+			}
+		}
+		
+	}
+	
+}//end Ebor_Options
+
+if(!( function_exists('ebor_additional_classes') )){
+	function ebor_additional_classes(){
+		
+		class Ebor_Customizer_Textarea_Control extends WP_Customize_Control {
+		    public $type = 'textarea';
+		    public function render_content() {
+		    ?>
+		        <label>
+		        	<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+		        	<textarea rows="3" style="width:100%;" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
+		        </label>
+		    <?php
+		    }
+		}
+		
+		class Ebor_Customizer_Number_Control extends WP_Customize_Control {
+			public $type = 'number';
+			public function render_content() {
+			?>
+				<label>
+					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+					<input type="number" <?php $this->link(); ?> value="<?php echo intval( $this->value() ); ?>" />
+				</label>
+			<?php
+			}
+		}
+		
+		class Ebor_Customizer_Demo_Import_control extends WP_Customize_Control {
+			public $type = 'demo-import';
+			public function render_content() {
+			?>
+				<label>
+					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+					<p>This will import all demo data. If this is not a fresh WordPress install (existing content) please make site & database backups before importing demo content.</p>
+					<p><strong>The import process will take up to 15 minutes depening on your server, start it & go grab a cup of tea!<br />Do not leave this page until you have confirmation of the import.</strong></p>
+					<a href="#" id="demo-import" class="btn">Import Demo Data</a>
+				</label>
+			<?php
+			}
+		}
+		
+	}
+}
--- index.php
+++ index.php
@@ -8,3 +8,69 @@ Version: 1.0.0
 Author: Tom Rhodes
 Author URI: http://www.madeinebor.com
 */	
+
+/**
+ * Plugin definitions
+ */
+define( 'EBOR_FRAMEWORK_PATH', trailingslashit(plugin_dir_path(__FILE__)) );
+define( 'EBOR_FRAMEWORK_VERSION', '1.0.0');
+
+/**
+ * Grab page builder, ensure that aqua page builder isn't installed seperately
+ */
+if(!( class_exists( 'AQ_Page_Builder' ) )){
+	require_once( EBOR_FRAMEWORK_PATH . 'page-builder/aqua-page-builder.php' );	
+}
+
+/**
+ * Grab simple options class
+ */
+require_once( EBOR_FRAMEWORK_PATH . 'ebor_options.php' );
+
+/**
+ * Grab all custom post type functions
+ */
+require_once( EBOR_FRAMEWORK_PATH . 'ebor_cpt.php' );
+
+/**
+ * ebor_framework_theme_support
+ * 
+ * The framework supports a number of custom posts types and features, all of these are initialised from within a theme by using add_theme_support()
+ * If a theme does not declare ebor-framework support then all features are loaded to avoid data loss.
+ * 
+ * @since 1.0.0
+ * @author tommusrhodus
+ */
+if(!( function_exists('ebor_framework_theme_support') )){
+	function ebor_framework_theme_support(){
+		
+		( current_theme_supports('ebor-framework') ) ? $supported = true : $supported = false;
+		
+		/**
+		 * Hook CPT registers to init
+		 * Check that we're using an ebor-framework theme and register functions accordingly.
+		 * If this isn't an ebor-framework theme (register is false) then we'll just enable everything.
+		 */
+		if( current_theme_supports('ebor-framework-portfolio') || $supported == false ){
+			add_action( 'init', 'ebor_framework_register_portfolio' );
+			add_action( 'init', 'ebor_framework_create_portfolio_taxonomies' );
+		}
+		
+		if( current_theme_supports('ebor-framework-team') || $supported == false ){
+			add_action( 'init', 'ebor_framework_register_team' );
+			add_action( 'init', 'ebor_framework_create_team_taxonomies' );
+		}
+		
+		if( current_theme_supports('ebor-framework-client') || $supported == false ){
+			add_action( 'init', 'ebor_framework_register_client' );
+			add_action( 'init', 'ebor_framework_create_client_taxonomies' );
+		}
+		
+		if( current_theme_supports('ebor-framework-testimonial') || $supported == false ){
+			add_action( 'init', 'ebor_framework_register_testimonial' );
+			add_action( 'init', 'ebor_framework_create_testimonial_taxonomies' );
+		}
+
+	}
+	add_action('after_setup_theme', 'ebor_framework_theme_support', 15);
+}
