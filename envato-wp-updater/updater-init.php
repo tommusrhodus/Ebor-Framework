1f7e0f86e58015bf163c2bbc9db693ce9df8c87e
TommusRhodus <hello@madeinebor.com>
2017-04-20 20:27:18 -0700

    Added basic updater instructions

--- envato-wp-updater/updater-init.php
+++ envato-wp-updater/updater-init.php
@@ -10,7 +10,7 @@ if( class_exists('Ebor_Options') ){
 	
 	$framework_options->add_panel('Ebor Framework: Update Settings', 1, '');
 	
-	$framework_options->add_section('framework_user_settings_section', 'User Settings', 30, 'Ebor Framework: Update Settings', '');
+	$framework_options->add_section('framework_user_settings_section', 'User Settings', 30, 'Ebor Framework: Update Settings', 'Please enter your Envato username exactly, case sensitive. For the API key visit the "settings" page of your Themeforest account, see the left menu for the "API Keys" option, enter a name and create a new key.');
 	$framework_options->add_setting('input', 'ebor_framework_username', 'Your Envato Username', 'framework_user_settings_section', '', 10);
 	$framework_options->add_setting('input', 'ebor_framework_api_key', 'Your Envato API Key', 'framework_user_settings_section', '', 15);
 	
