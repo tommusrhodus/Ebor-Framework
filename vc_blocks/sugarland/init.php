b644529ec8705806cf58b5f196c534ac90fc6b2d
TommusRhodus <hello@madeinebor.com>
2017-09-27 16:09:27 -0700

    Sugarland Fix

--- vc_blocks/sugarland/init.php
+++ vc_blocks/sugarland/init.php
@@ -4,11 +4,11 @@
  * Custom blocks for visual composer in this theme.
  */
 function ebor_framework_register_sugarland_blocks(){
-	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/vc_maps_button_block.php');
-	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/vc_video_button_block.php');
-	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/vc_portfolio_block.php');
-	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/vc_team_block.php');
-	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/vc_pricing_table_block.php');
+	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/sugarland/vc_maps_button_block.php');
+	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/sugarland/vc_video_button_block.php');
+	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/sugarland/vc_portfolio_block.php');
+	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/sugarland/vc_team_block.php');
+	require_once( EBOR_FRAMEWORK_PATH . 'vc_blocks/sugarland/vc_pricing_table_block.php');
 }
 
 function ebor_framework_sugarland_init(){
