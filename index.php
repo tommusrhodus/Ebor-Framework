80fa0fae80d1997a0bb614c38f35ca6ce6f12c7c
TommusRhodus <hello@madeinebor.com>
2017-12-13 15:47:13 -0800

    Version Bump For Candar

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 Plugin Name: Ebor Framework
 Plugin URI: http://www.tommusrhodus.com/ebor-framework/
 Description: Ebor Framework - The Driving Force Behind TommusRhodus Themes
-Version: 1.4.5
+Version: 1.4.6
 Author: Tom Rhodes
 Author URI: http://www.tommusrhodus.com/
 */	
@@ -13,7 +13,7 @@ Author URI: http://www.tommusrhodus.com/
  * Plugin definitions
  */
 define( 'EBOR_FRAMEWORK_PATH', trailingslashit(plugin_dir_path(__FILE__)) );
-define( 'EBOR_FRAMEWORK_VERSION', '1.4.5');
+define( 'EBOR_FRAMEWORK_VERSION', '1.4.6');
 
 /**
  * Styles & Scripts
